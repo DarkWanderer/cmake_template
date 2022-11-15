f8f864772f249651fd97605687bb76e7ddf0040b
DarkWanderer <elquentaro@gmail.com>
2022-11-15 23:57:22 +0100

    Apply autopep8 to generator

--- generate-cmakepresets.py
+++ generate-cmakepresets.py
@@ -1,19 +1,21 @@
 import json
 import argparse
 
+
 def get_base_configure_preset(generator: str):
     return {
         "name": generator.lower() + "-base",
-        "hidden": True, 
+        "hidden": True,
         "generator": "Unix Makefiles" if generator == 'Make' else generator,
-        "binaryDir": "${sourceDir}/out/build/"+generator.lower()+"/${presetName}",
+        "binaryDir": "${sourceDir}/out/build/" + generator.lower() + "/${presetName}",
         "installDir": "${sourceDir}/out/install/${presetName}"
     }
+
 
 def get_os_base_configure_preset(os: str, inherits: str):
     result = {
         "name": os.lower() + "-base",
-        "hidden": True, 
+        "hidden": True,
         "inherits": inherits,
         "condition": {
             "type": "equals",
@@ -22,32 +24,48 @@ def get_os_base_configure_preset(os: str, inherits: str):
         }
     }
     if os == 'Windows':
-        result["cacheVariables"] = { "CMAKE_C_COMPILER": "cl.exe", "CMAKE_CXX_COMPILER": "cl.exe" }
+        result["cacheVariables"] = {
+            "CMAKE_C_COMPILER": "cl.exe",
+            "CMAKE_CXX_COMPILER": "cl.exe"
+        }
     else:
-        result["cacheVariables"] = { "CMAKE_C_COMPILER": "gcc", "CMAKE_CXX_COMPILER": "g++" }
+        result["cacheVariables"] = {
+            "CMAKE_C_COMPILER": "gcc",
+            "CMAKE_CXX_COMPILER": "g++"
+        }
         result["vendor"] = {
-            "microsoft.com/VisualStudioSettings/CMake/1.0": { "hostOS": [os] },
-            "microsoft.com/VisualStudioRemoteSettings/CMake/1.0": {"sourceDir": "$env{HOME}/.vs/$ms{projectDirName}" }
+            "microsoft.com/VisualStudioSettings/CMake/1.0": {"hostOS": [os]},
+            "microsoft.com/VisualStudioRemoteSettings/CMake/1.0": {"sourceDir": "$env{HOME}/.vs/$ms{projectDirName}"}
         }
     return result
+
 
 def get_os_preset(os: str, inherits: str, arch: str, conf: str):
     result = {
         "name": os.lower() + "-" + arch + "-" + conf.lower(),
         "inherits": inherits,
         "displayName": arch + " " + conf,
-        "architecture": { "value": arch, "strategy": "external" },
-        "cacheVariables": { 
+        "architecture": {"value": arch, "strategy": "external"},
+        "cacheVariables": {
             "CMAKE_BUILD_TYPE": "Debug" if conf == "ASan" else conf,
             "ASAN_ENABLED": conf == "ASan"
         }
     }
     return result
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog = 'generate-cmakepresets', description = 'Generates CMakePresets.json file with custom parameters')
-    parser.add_argument('--file', default='CMakePresets.json')           # positional argument
-    parser.add_argument('--generator', default='Ninja', choices=['Ninja', 'Make'])
+    parser = argparse.ArgumentParser(
+        prog='generate-cmakepresets',
+        description='Generates CMakePresets.json file with custom parameters'
+    )
+    parser.add_argument('--file', default='CMakePresets.json')
+    parser.add_argument(
+        '--generator',
+        default='Ninja',
+        choices=[
+            'Ninja',
+            'Make'])
     parser.add_argument('--macos', action='store_true')
     args = parser.parse_args()
     print(args)
@@ -57,37 +75,12 @@ if __name__ == "__main__":
     test_presets = []
 
     base_configure_preset = get_base_configure_preset(args.generator)
-    configure_presets.append(base_configure_preset)
-
-
-    os_names = ["Linux", "Windows", "macOS"] if args.macos else ["Linux", "Windows"]
-
-    for os in os_names:
-        os_base_configure_preset = get_os_base_configure_preset(os, base_configure_preset["name"])
-        configure_presets.append(os_base_configure_preset)
-        for conf in ["Debug", "Release", "ASan"]:
-            configure_presets.append(get_os_preset(os,os_base_configure_preset["name"],"x64",conf))
-            if os == "Windows":
-                configure_presets.append(get_os_preset(os,os_base_configure_preset["name"],"x86",conf))
-    
-    base_build_preset = { 
-        'name': 'build-base', 
+    base_build_preset = {
+        'name': 'build-base',
         'hidden': True,
         'jobs': 1,
         'cleanFirst': False
     }
-    build_presets.append(base_build_preset)
-
-    for conf_preset in configure_presets:
-        if conf_preset.get('hidden', False) == True:
-            continue
-        build_presets.append({
-            "name": conf_preset["name"],
-            "inherits": base_build_preset["name"],
-            "displayName": conf_preset["displayName"],
-            "configurePreset": conf_preset["name"],
-        })
-
     base_test_preset = {
         "name": "test-base",
         "description": "Basic shared test settings",
@@ -100,11 +93,45 @@ if __name__ == "__main__":
             "outputOnFailure": True
         }
     }
+    configure_presets.append(base_configure_preset)
+    build_presets.append(base_build_preset)
     test_presets.append(base_test_preset)
+
+    os_names = [
+        "Linux",
+        "Windows",
+        "macOS"] if args.macos else [
+        "Linux",
+        "Windows"]
+
+    for os in os_names:
+        os_base_configure_preset = get_os_base_configure_preset(
+            os, base_configure_preset["name"])
+        configure_presets.append(os_base_configure_preset)
+        for conf in ["Debug", "Release", "ASan"]:
+            configure_presets.append(
+                get_os_preset(
+                    os,
+                    os_base_configure_preset["name"],
+                    "x64",
+                    conf))
+            if os == "Windows":
+                configure_presets.append(
+                    get_os_preset(
+                        os,
+                        os_base_configure_preset["name"],
+                        "x86",
+                        conf))
 
     for conf_preset in configure_presets:
         if conf_preset.get('hidden', False) == True:
             continue
+        build_presets.append({
+            "name": conf_preset["name"],
+            "inherits": base_build_preset["name"],
+            "displayName": conf_preset["displayName"],
+            "configurePreset": conf_preset["name"],
+        })
         test_presets.append({
             "name": conf_preset["name"],
             "inherits": base_test_preset["name"],
